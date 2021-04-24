6bf4aa16784b58ed92d1b559bbc1a4c1b98161f5
xhemajl <xhemajl@post.com>
2021-04-24 11:58:33 +0200

    Test price

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,9 +1,19 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.service.PriceException;
+import com.udacity.pricing.service.PricingService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +21,23 @@ public class PricingServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test(expected = PriceException.class)
+	public void exceptionThrownWhenNonExistingId() throws PriceException {
+		Long nonExistingVehicleId = -1L;
+		PricingService.getPrice(nonExistingVehicleId);
+	}
+
+	@Test
+	public void priceIsCorrect() throws PriceException {
+		Long vehicleId = 1L;
+		Price price = PricingService.getPrice(vehicleId);
+
+		assertThat(price, is(notNullValue()));
+		assertThat(price.getVehicleId(), is(equalTo(vehicleId)));
+		assertThat(price.getCurrency(), is(equalTo("USD")));
+		assertThat(price.getPrice(), is(greaterThan(BigDecimal.ZERO)));
 	}
 
 }
